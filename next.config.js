62e9a18a64dbdd75887844040d574278e6cd6d0e
Misharin <1200800my@gmail.com>
2025-07-18 19:27:07 +0300

    chore: add next.config.js and remove pages folder

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const { i18n } = require('./next-i18next.config')
-
-/** @type {import('next').NextConfig} */
 module.exports = {
   experimental: {
     appDir: true,
   },
-  i18n,
+  i18n: {
+    defaultLocale: 'uk',
+    locales: ['uk', 'en', 'ru'],
+  },
 }
